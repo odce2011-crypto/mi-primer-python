957f7ef3adb65fb79896dd6dd86b3de2e1bb2d78
odce2011-crypto <odce2011@gmail.com>
2026-01-03 12:20:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola oscarito! soy una pagina web."
+    return "¡Hola oscarito! ya se que quieres crear una serie."
 
 if __name__ == "__main__":
     # Esto le pregunta al sistema: ¿Qué puerto tienes libre? Si no hay uno, usa el 5000.
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
