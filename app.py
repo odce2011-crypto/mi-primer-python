3aeb395f75eb5848b25397743a154e74c08d8ba8
odce2011-crypto <odce2011@gmail.com>
2026-01-03 11:58:53 -0600

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "<h1>¡Hola Mundo!</h1><p>Mi Python está corriendo en Hostinger con EasyPanel.</p>"
+
+if __name__ == "__main__":
+    # Importante usar el puerto que EasyPanel espera o definir uno
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
